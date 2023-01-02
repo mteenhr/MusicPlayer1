504888b076dbdfe71ea2f7491739a8bd1390b132
mteenhr <121768704+mteenhr@users.noreply.github.com>
2023-01-02 12:21:20 +0330

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,8 @@ class Config:
         EDIT_TITLE=None
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")     
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     SESSION = os.environ.get("SESSION_STRING", "")
     playlist=[]
     msg = {}
